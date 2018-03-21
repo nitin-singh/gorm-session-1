f99402521eecf8a585165449a54bc94901bfb2ce
Nitin Kumar Singh <nitin.kumar@tothenew.com>
2018-03-21 13:31:05 +0530

    Added code in bootstrap for findsavecreate demo5

--- grails-app/controllers/gormdemo1/DemoController.groovy
+++ grails-app/controllers/gormdemo1/DemoController.groovy
@@ -7,14 +7,7 @@ class DemoController {
     def index() {}
 
     def demo() {
-        User user = new User(name: "Nitin", email: "nitin.kumar@tothenew.com", dob: new Date() - 100, balance: 555)
-        user.name = null
-        user.validate()
         Map response = [
-                email    : user.validate(['email']),
-                name     : user.validate(['name']),
-                hasErrors: user.hasErrors(),
-                allErrors: user.errors.allErrors
         ]
         render(response as JSON)
     }
