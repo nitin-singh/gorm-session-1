65d98961e84ccfdbf0a944b16b73c5677165ec38
Nitin Kumar Singh <nitin.kumar@tothenew.com>
2018-03-21 15:45:59 +0530

    Added demo code in controller and bootstrap.

--- grails-app/controllers/gormdemo1/DemoController.groovy
+++ grails-app/controllers/gormdemo1/DemoController.groovy
@@ -6,16 +6,48 @@ class DemoController {
 
     def index() {}
 
+    def init() {
+        User user = new User(name: "Nitin", dob: new Date() - 1000, balance: 1000, email: "nitin.kumar@tothenew.com")
+        10.times {
+            user.addToBooks(new Book(name: "Book ${it}"))
+        }
+
+        user.save(flush: true)
+        render "Created Data"
+    }
+
     def demo() {
-        User user = new User(name: "Nitin", email: "nitin.kumar@tothenew.com", dob: new Date() - 100, balance: 555)
-        user.name = null
-        user.validate()
+        //removing book only from collection
+        User u = User.get(1)
+        Book b = Book.get(1)
+        u.removeFromBooks(b)
+        u.save(flush: true)
         Map response = [
-                email    : user.validate(['email']),
-                name     : user.validate(['name']),
-                hasErrors: user.hasErrors(),
-                allErrors: user.errors.allErrors
+                user1Books: u.books*.id.sort(),
+                allBooks  : Book.list()*.id.sort()
         ]
         render(response as JSON)
+    }
+
+    def demo2() {
+//        removing book from collection and also deleting it.
+        User u = User.get(1)
+        Book b = Book.get(1)
+        u.removeFromBooks(b)
+        b.delete()
+        u.save(flush: true)
+        Map response = [
+                user1Books: u.books*.id.sort(),
+                allBooks  : Book.list()*.id.sort()
+        ]
+        render(response as JSON)
+    }
+
+    def demo3() {
+//        list all books.
+        Map response = [
+                user1Books: User.get(1).books*.id.sort()
+        ]
+        render response as JSON
     }
 }
--- grails-app/domain/gormdemo1/User.groovy
+++ grails-app/domain/gormdemo1/User.groovy
@@ -1,16 +1,20 @@
 package gormdemo1
 
 class User {
+
     String name
     String email
     Date dob
+    Boolean active = false
     Integer balance = 0
+
+    static hasMany = [friends: User, books: Book]
 
     static constraints = {
         email(nullable: true)
         dob(nullable: true)
     }
-    
+
     def afterInsert() {
         log.info "----------Into After Insert------"
     }
